abc2db2e40937edc149d3079cb62c560630f705c
joao-vieira-mobietrain <joao.vieira@mobietrain.com>
2023-07-14 14:08:29 +0100

    chore: using chartbar instead.

--- frontend/src/containers/LepaStatistics.tsx
+++ frontend/src/containers/LepaStatistics.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { SectionTitle } from '../components/SectionTitle';
-import { PieChart } from '../components/PieChart';
+import { BarChart } from '../components/BarChart';
 
-// TODO: Think how to improve it
 export const LepaStatistics: React.FC<any> = ({ data }) => {
   const [finalData, setFinalData] = React.useState<any>([]);
 
   React.useEffect(() => {
     const intervals = Array(21).fill(1).map((n, i) => i * 5);
+    
     const dataByIntervalsMap = data.reduce((acc: any, account: any) => {
       const { total } = account;
       for (const value of Array.from(Array(intervals.length).keys())) {
@@ -21,18 +21,26 @@ export const LepaStatistics: React.FC<any> = ({ data }) => {
       }
       return acc;
     }, {});
-    const finalData = Object.keys(dataByIntervalsMap).map(interval => ({
-        interval,
+    
+    const finalData = Object.keys(dataByIntervalsMap).map((interval) => {
+      const intervalIndex = intervals.findIndex(x => x === parseInt(interval, 10));
+      if (intervalIndex === -1) return null;
+      const nextIntervalIndex = intervalIndex + 1;
+      return {
+        interval: intervals[nextIntervalIndex] ? `${parseInt(interval, 10) + 1}-${intervals[nextIntervalIndex]}` : `>${interval}`,
         numberOfLepas: dataByIntervalsMap[interval]
-    }));
+      };
+    });
+    
     setFinalData(finalData);
   }, [data]);
 
   return finalData.length ?
     <>
       <SectionTitle title='LEPA Statistics' />
-      <div className='flex justify-center mb-24'>
-        <PieChart data={finalData} dataKey='numberOfLepas' nameKey='interval' />
+      <div className='flex flex-col items-center justify-center mb-24'>
+        <h2>Published learning paths by accounts</h2>
+        <BarChart data={finalData} dataKey='numberOfLepas' xAxisKeys='interval' layout='horizontal' name='Number of accounts' />
       </div>
     </> : null;
 };
